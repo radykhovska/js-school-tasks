29f5b476396467dee454e0a2e0393f0bf54431d6
Radykhovska <zagnibeda@i.ua>
2018-06-28 21:22:00 +0300

    Task 1 done

--- task-1/index.js
+++ task-1/index.js
@@ -7,7 +7,43 @@
 function fillSpiralMatrix(n) {
   const result = [[]];
 
-  // Ваш код
+  for (let i = 0; i < n; i++) {
+    result[i] = [];
+    for (let j = 0; j < n; j++) {
+      result[i][j] = 0;
+    }
+  }
+  
+  let side = n;
+  let offset = 0;
+  let currentNumber = 1;
+
+  while (offset < (n/2)) {
+    for (let i = 0; i < side; i++) {
+        result[offset][offset+i] = currentNumber;
+        currentNumber++;
+    }
+
+    for (let i = 0; i < side-2; i++) {
+        result[1+offset+i][n-1-offset] = currentNumber;
+        currentNumber++;
+    }
+
+    if ((n/2 - offset > 0) && (side > 1))  {
+        for (let i = 0; i < side; i++) {
+            result[n-1-offset][n-1-i-offset] = currentNumber;
+            currentNumber++;
+        }
+    }
+
+    for (let i = 0; i < side-2; i++) {
+        result[n-2-offset-i][offset] = currentNumber;
+        currentNumber++;
+    }
+
+    offset++;
+    side-=2;
+  }
 
   return result;
 }
